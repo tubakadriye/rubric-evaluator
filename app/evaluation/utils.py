4d58f09a094d078189c5f3ccfb7ef4ad7bb58115
tubakadriye <tubakadriye17@gmail.com>
2026-04-26 18:56:01 +0200

    update grade scores including all models

--- app/evaluation/utils.py
+++ app/evaluation/utils.py
@@ -1,6 +1,12 @@
-def extract_scores(runs, model="openai"):
-    return [
-        r[model]["final_grade"]
-        for run in runs
-        for r in run
-    ]
+def extract_scores(runs, mode="all"):
+    scores = []
+
+    for run in runs:
+        for r in run:
+            if mode == "all":
+                for model in r:
+                    scores.append(r[model]["final_grade"])
+            else:
+                scores.append(r[mode]["final_grade"])
+
+    return scores
